14600a25e1b8f61e6dbb23d23f3b60672d870824
seniordeveloper0817 <102760454+seniordeveloper0817@users.noreply.github.com>
2022-06-28 01:36:07 +0200

    bb

--- js/addToCart.js
+++ js/addToCart.js
@@ -8,7 +8,6 @@ const addToCartButton = document.getElementsByClassName('addToCart');
 const badge = document.getElementById('Badge');
 const cartBody = document.getElementById('cart-pop').querySelector('table');
 
-
 window.addEventListener("DOMContentLoaded", (evt) => {
     //add to cart
     for (let i = 0; i < addToCartButton.length; i++) {
